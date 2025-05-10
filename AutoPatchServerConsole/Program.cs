d43915539010fa033115dc933e3585ae4935ded6
DuaSelipar <138177993+duaselipar@users.noreply.github.com>
2025-05-10 20:59:12 +0800

    Update

--- AutoPatchServerConsole/Program.cs
+++ AutoPatchServerConsole/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-class AutoPatchServer
+class AutoPatchServerConsole
 {
     static void PrintBanner()
     {
